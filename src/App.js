465b859a505cc0d2286d5ec025a1ed6d85734475
GaneshShaw123 <106146594+GaneshShaw123@users.noreply.github.com>
2022-05-28 13:23:59 +0530

    Dark Theam added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState}  from 'react'
 
-function app() {
+
+import Navbar from './Componenets/Navbar';
+import Footer from './Componenets/Footer';
+import Routes from './Componenets/Routes';
+
+
+
+
+function App() {
+
+  const [darkTheme, setDarkTheme] = useState(false);
   return (
-    <div>app</div>
+    <div className={darkTheme ? 'dark' : ''}>
+      <div className="dark:bg-gray-900 bg-gray-100 dark:text-gray-200 black min-h-screen">
+        <Navbar setDarkTheme={setDarkTheme} darkTheme={darkTheme}/>
+        <Routes />
+        <Footer />
+      </div>
+    </div>
   )
 }
 
-export default app
+export default App
